47fb6e902730d88c591ec3340bfe36567031b2d1
Jin Hu <bixuehujin@gmail.com>
2014-02-03 12:55:01 +0800

    Added test for insertId

--- tests/React/Tests/NoResultQueryTest.php
+++ tests/React/Tests/NoResultQueryTest.php
@@ -3,20 +3,22 @@
 namespace React\Tests;
 
 use React\MySQL\Query;
+
+
 class NoResultQueryTest extends  BaseTestCase{
-	
+
 	public function testUpdateSimple() {
 		$loop = \React\EventLoop\Factory::create();
-		
+
 		$connection = new \React\MySQL\Connection($loop, array(
 			'dbname' => 'test',
 			'user'   => 'test',
 			'passwd' => 'test',
 		));
-		
+
 		$connection->connect(function (){});
 		$that  = $this;
-		
+
 		$connection->query('update book set created=999 where id=1', function ($command, $conn) use ($loop){
 			$this->assertEquals(false, $command->hasError());
 			$this->assertEquals(1, $command->affectedRows);
@@ -24,5 +26,25 @@ class NoResultQueryTest extends  BaseTestCase{
 		});
 		$loop->run();
 	}
-	
+
+	public function testInsertSimple(){
+	    $loop = \React\EventLoop\Factory::create();
+
+	    $connection = new \React\MySQL\Connection($loop, array(
+	        'dbname' => 'test',
+	        'user'   => 'test',
+	        'passwd' => 'test',
+	    ));
+
+	    $connection->connect(function (){});
+	    $that  = $this;
+
+	    $connection->query("insert into book (`name`) values('foo')", function ($command, $conn) use ($loop){
+	        $this->assertEquals(false, $command->hasError());
+	        $this->assertEquals(1, $command->affectedRows);
+	        $this->assertEquals(3, $command->insertId);
+	        $loop->stop();
+	    });
+	    $loop->run();
+	}
 }
