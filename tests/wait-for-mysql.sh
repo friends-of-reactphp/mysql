3c2ecb56c47bd177f7f9f4153b763637bbddba52
Simon Frings <simon@clue.engineering>
2023-11-06 09:02:03 +0100

    Improve CI workflow to await database

--- tests/wait-for-mysql.sh
+++ tests/wait-for-mysql.sh
@@ -3,6 +3,6 @@
 CONTAINER="mysql"
 USERNAME="test"
 PASSWORD="test"
-while ! docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
+while ! docker exec $CONTAINER mysql --host=127.0.0.1 --port=3306 --user=$USERNAME --password=$PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
     sleep 1
 done
